124967f242397fab2cadb887f3ce9327077fa284
6035512070 <plant.lokiz@gmail.com>
2020-10-02 23:59:24 +0700

    5th commit

--- App.js
+++ App.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import ZipCodeScreen from './component/ZipCode';
+import ZipCodeScreen from './component/ZipCodeScreen';
 import WeatherScreen from './component/WeatherScreen';
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  return (        
+  return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={ZipCodeScreen} />
         <Stack.Screen name="Weather" component={WeatherScreen} />
-      </Stack.Navigator> 
-    </NavigationContainer>       
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
